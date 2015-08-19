aeb1f1ba726a9353885db87f4e81ec53e83f8d31
ksharpdabu <ksharpdabu123@gmail.com>
2015-08-19 23:07:24 +0800

    Initial commit

---
+++ app/src/main/java/com/dabu/circleimage/MainActivity.java
@@ -0,0 +1,26 @@
+package com.dabu.circleimage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView image;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView)findViewById(R.id.imageview);
+
+        Picasso.with(this).load(R.mipmap.cat03).transform(new CircleTransform()).into(image);
+    }
+
+
+}
